32cd15fc26f4b2ea7cdbdafe422967a3d5dd388b
nicolas-montealegre <nmontealegre@contractor.renuityhome.com>
2024-08-23 11:28:31 -0500

    Added: translations

---
+++ Resources/lang/es/cms.php
@@ -0,0 +1,48 @@
+<?php
+
+return [
+  "sidebar" => [
+    "adminGroup" => "Tareas",
+    "adminCategories" => "Categorías",
+    "adminPriorities" => "Prioridades",
+    "adminStatuses" => "Estados",
+    "adminTasks" => "Tareas"
+  ],
+  "label" => [],
+  "form" => [
+    "priority" => "Prioridad",
+    "estimatedTime" => "Tiempo estimado",
+    "assigned" => "Asignado",
+    "deletedAt" => "Eliminado en",
+    "color" => "Color",
+  ],
+  "newTask" => "Nueva Tarea",
+  "updateTask" => "Actualizar Tarea",
+  "newPriority" => "Nueva Prioridad",
+  "updatePriority" => "Actualizar Prioridad",
+  "newCategory" => "Nueva Categoría",
+  "updateCategory" => "Actualizar Categoría",
+  "newStatus" => "Nuevo Estado",
+  "updateStatus" => "Actualizar Estado",
+  "taskManagement" => "Gestión de Tareas",
+  "duration" => "Duración",
+  "week" => "Semana",
+  "previousWeek" => "Semana anterior",
+  "nextWeek" => "Próxima semana",
+  "timeLogs" => [
+    "title" => "Registros de tiempo",
+    "total" => "Tiempo total registrado",
+    "startDate" => "Fecha de inicio",
+    "timeSpent" => "Tiempo empleado",
+    "format" => [
+      "title" => "Por favor use este formato => 2w 4d 6h 45m",
+      "weeks" => "w = semanas",
+      "days" => "d = días",
+      "hours" => "h = horas",
+      "minutes" => "m = minutos"
+    ]
+  ],
+  "date" => [
+    "error" => "La fecha de inicio es mayor que la fecha de finalización"
+  ]
+];
