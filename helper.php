fcd909ab10e6f051cc9d53e267beab04ef845c12
michael <msolano2505@gmail.com>
2024-09-15 17:36:52 -0500

    removed helper `humanizeDuration`

--- helper.php
+++ helper.php
@@ -45,25 +45,3 @@ if (! function_exists('convertMinutesToHumanReadable')) {
       return $result;
     }
 }
-
-if (! function_exists('humanizeDuration')) {
-    function humanizeDuration($startDate, $endDate)
-    {
-        if (!$endDate) {
-            return null;
-        }
-
-        $start = Carbon::parse($startDate);
-        $end = Carbon::parse($endDate);
-
-        $diffInMinutes = $start->diffInMinutes($end);
-
-        $days = floor($diffInMinutes / (60 * 24));
-        $hours = floor(($diffInMinutes % (60 * 24)) / 60);
-        $minutes = $diffInMinutes % 60;
-
-        return ($days > 0 ? $days . ' ' . trans('itask::tasks.days') . ' ' : '') . 
-               ($hours > 0 ? $hours . ' ' . trans('itask::tasks.hours') . ' ' : '') . 
-               ($minutes > 0 ? $minutes . ' ' . trans('itask::tasks.minutes') : '');
-    }
-}
