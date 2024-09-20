c507b760244c0910f1bdd52d955625bac556d11e
michael <msolano2505@gmail.com>
2024-09-19 21:58:53 -0500

    moved the helper convertMinutesToHumanReadable

--- helper.php
+++ helper.php
@@ -10,38 +10,3 @@ if (! function_exists('convertMinutesToSeconds')) {
         return $minutes * 60;
     }
 }
-
-if (! function_exists('convertMinutesToHumanReadable')) {
-    function convertMinutesToHumanReadable($minutes)
-    {
-        if (!$minutes) return 0;
-        if ($minutes < 60) {
-            return $minutes . ' ' . trans('itask::tasks.minutes');
-        }
-
-      $weeks = floor($minutes / (60 * 24 * 7));
-      $days = floor(($minutes % (60 * 24 * 7)) / (60 * 24));
-      $hours = floor(($minutes % (60 * 24)) / 60);
-      $remainingMinutes = $minutes % 60;
-
-      $result = '';
-
-      if ($weeks > 0) {
-        $result .= $weeks . ' ' . trans('itask::tasks.weeks') . ' ';
-      }
-
-      if ($days > 0) {
-        $result .= $days . ' ' . trans('itask::tasks.days') . ' ';
-      }
-
-      if ($hours > 0) {
-        $result .= $hours . ' ' . trans('itask::tasks.hours') . ' ';
-      }
-
-      if ($remainingMinutes > 0) {
-        $result .= trans('itask::tasks.and') . ' ' . $remainingMinutes . ' ' . trans('itask::tasks.minutes');
-      }
-
-      return $result;
-    }
-}
